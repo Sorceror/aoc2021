7149224cc6d95034f84443f7bfbe8193f9646c38
Pavel Janecka <janecka.pavel@gmail.com>
2021-12-01 16:42:10 +0100

    Day1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
+    // https://adventofcode.com/2021/day/1
+    // count the number of times a depth measurement increases from the previous measurement
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .windowed(2)
+            .fold(0) { acc, ints -> if (ints[0] < ints[1]) acc + 1 else acc }
     }
 
+    // count the number of times the sum of measurements in this sliding window increases from the previous sum
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { it.toInt() }
+            .windowed(3)
+            .map { it.sum() }
+            .windowed(2)
+            .fold(0) { acc, ints -> if (ints[0] < ints[1]) acc + 1 else acc }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
