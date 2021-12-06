278418e83d636b8c7f2c037ee55649873dd85d73
Pavel Janecka <janecka.pavel@gmail.com>
2021-12-06 13:15:51 +0100

    Day6 - Added original array evolution version that is perfectly viable for part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -13,19 +13,22 @@ fun main() {
     // So, each fish as a single number that represents the number of days until it creates a new lanternfish.
     // Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day.
     // How many lanternfish would there be after 80 days?
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
         // list/array evolution is valid solution until the size of it an issue
-        val days = 80
-        val fishes = parseInput(input)
-        for (day in 0 until days) {
-            val breedingCount = fishes[0]
-            for (fIdx in 1 until fishes.size) {
-                fishes[fIdx - 1] = fishes[fIdx]
+        var fishes = input.first().split(',').map { it.toInt() }.toTypedArray()
+        for(day in 1..80) {
+            var tmpArr = emptyArray<Int>()
+            for (fIdx in fishes.indices) {
+                if (fishes[fIdx] == 0) {
+                    fishes[fIdx] = 6
+                    tmpArr += 8
+                } else {
+                    fishes[fIdx] = fishes[fIdx] - 1
+                }
             }
-            fishes[8] = breedingCount
-            fishes[6] = fishes[6] + breedingCount
+            fishes += tmpArr
         }
-        return fishes.sum()
+        return fishes.size
     }
 
     // How many lanternfish would there be after 256 days?
@@ -45,7 +48,7 @@ fun main() {
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5934L)
+    check(part1(testInput) == 5934)
     check(part2(testInput) == 26984457539)
 
     val input = readInput("Day06")
