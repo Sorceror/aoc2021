ea2cf37cb9e029ab08266532c9fe13db4a6c4157
Pavel Janecka <janecka.pavel@gmail.com>
2021-12-06 10:13:11 +0100

    Day6 complete

---
+++ src/Day06.kt
@@ -0,0 +1,54 @@
+fun main() {
+    fun parseInput(input: List<String>) = input.first()
+        .split(',')
+        .map { it.toLong() }
+        // maps number of days for each fish to array of frequencies
+        .fold(LongArray(9)) {
+            arr, v -> arr[v.toInt()] = arr[v.toInt()] + 1
+            arr
+        }
+
+    // https://adventofcode.com/2021/day/6
+    // One lanternfish might have 2 days left until it creates another lanternfish, while another might have 4.
+    // So, each fish as a single number that represents the number of days until it creates a new lanternfish.
+    // Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day.
+    // How many lanternfish would there be after 80 days?
+    fun part1(input: List<String>): Long {
+        // list/array evolution is valid solution until the size of it an issue
+        val days = 80
+        val fishes = parseInput(input)
+        for (day in 0 until days) {
+            val breedingCount = fishes[0]
+            for (fIdx in 1 until fishes.size) {
+                fishes[fIdx - 1] = fishes[fIdx]
+            }
+            fishes[8] = breedingCount
+            fishes[6] = fishes[6] + breedingCount
+        }
+        return fishes.sum()
+    }
+
+    // How many lanternfish would there be after 256 days?
+    fun part2(input: List<String>): Long {
+        // instead of each fish frequencies of fishes' lifetime are evaluated
+        val days = 256
+        val fishes = parseInput(input)
+        for (day in 0 until days) {
+            val breedingCount = fishes[0]
+            for (fIdx in 1 until fishes.size) {
+                fishes[fIdx - 1] = fishes[fIdx]
+            }
+            fishes[8] = breedingCount
+            fishes[6] = fishes[6] + breedingCount
+        }
+        return fishes.sum()
+    }
+
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 5934L)
+    check(part2(testInput) == 26984457539)
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
